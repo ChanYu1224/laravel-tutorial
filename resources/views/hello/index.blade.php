82de70b065f5af02d50ee54708e7026b6e7024ff
Yuchn <kinkitunekin@gmail.com>
2022-05-30 14:32:58 +0900

    componentディレクティブを用いてコンポーネントを作成

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,6 +10,16 @@
 @section('content')
     <p>ここが本文のコンテンツです．</p>
     <p>必要なだけ記述出来ます．</p>
+
+    @component('components.message')
+        @slot('message_title')
+            CAUTION!
+        @endslot
+        
+        @slot('message_content')
+            これはメッセージの表示です．
+        @endslot
+    @endcomponent
 @endsection
 
 @section('footer')
